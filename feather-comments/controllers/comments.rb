a4fc36ec6fd3dcf3e4a9ad635129b09b3b612c65
Michael Leung <blamemike@gmail.com>
2008-04-02 22:23:37 -0400

    1.) Removed that class << self in comment.rb as I think the ruby idiom is usually for multiple class methods. Changed to self. 2.) Got rid of the <% Article.new %> in the comment view to keep the views light. 3.) Changed post_id in comments to article_id for consistency.

--- feather-comments/controllers/comments.rb
+++ feather-comments/controllers/comments.rb
@@ -2,6 +2,6 @@ class Comments < Application
   def create
     @comment = Comment.new(params[:comment])
     session[:comment_error] = @comment.errors if !@comment.save
-    redirect Article[@comment.post_id].permalink
+    redirect Article[@comment.article_id].permalink
   end
 end
--- feather-comments/models/comment.rb
+++ feather-comments/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < DataMapper::Base
-  property :post_id, :integer
+  property :article_id, :integer
   property :name, :string
   property :website, :string
   property :comment, :text
@@ -7,9 +7,8 @@ class Comment < DataMapper::Base
   
   validates_presence_of :name, :website, :comment
   
-  class << self
-    def all_for_post(post_id, method = :all)
-      self.send(method.to_s, {:post_id => post_id, :order => "created_at DESC"})
-    end
+  def self.all_for_post(article_id, method = :all)
+    self.send(method, {:article_id => article_id, :order => "created_at DESC"})
   end
+
 end
