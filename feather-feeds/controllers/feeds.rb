a78fdccb7ab82bbc03c72bed45aed35b827a54ae
El Draper <el@eldiablo.co.uk>
2008-04-05 16:48:15 +0100

    implemented RSS feeds, removed ATOM action for plugin (ATOM not supported), also added a hook so that a link to the feed is placed within the main blog template

--- feather-feeds/controllers/feeds.rb
+++ feather-feeds/controllers/feeds.rb
@@ -1,11 +1,32 @@
 class Feeds < Application
   def rss
-    @articles = Article.all(:limit => 15, :order => "published_at DESC")
-    #TODO: render RSS
+    @articles = Article.all(:published => true, :limit => 15, :order => "published_at DESC")
+    rss = ""
+    xml = Builder::XmlMarkup.new(:target => rss)
+    xml.instruct!
+    xml.rss "version" => "2.0" do
+      xml.channel do
+        xml.title         Configuration.first.title
+        xml.link          "http://#{request.env['HTTP_HOST']}#{request.uri}"
+        xml.pubDate       rfc822(@articles.first.published_at) if @articles.length > 0
+        xml.description   Configuration.first.tag_line
+        @articles.each do |article|
+          xml.item do
+            xml.title         article.title
+            xml.link          "http://#{request.env['HTTP_HOST']}#{article.permalink}"
+            xml.description   article.content
+            xml.pubDate       rfc822(article.published_at)
+            xml.guid          "http://#{request.env['HTTP_HOST']}#{article.permalink}"
+            xml.author        article.user.login
+          end
+        end
+      end
+    end
+    rss
   end
-  
-  def atom
-    @articles = Article.all(:limit => 15, :order => "published_at DESC")
-    #TODO: render ATOM
+
+  # Returns the specified time in RFC822 format
+  def rfc822(time)
+    time.strftime("%a, %d %b %Y %H:%M:%S GMT")
   end
 end
--- feather-feeds/init.rb
+++ feather-feeds/init.rb
@@ -1,17 +1,14 @@
 require File.join(File.join(File.dirname(__FILE__), "controllers"), "feeds")
+require "builder"
 
 Hooks::Routing.add_route do
   {:url => "/rss", :controller => "Feeds", :action => "rss" }
 end
 
-Hooks::Routing.add_route do
-  {:url => "/atom", :controller => "Feeds", :action => "atom" }
+Hooks::Menu.add_menu_item do
+  {:text => "Feed", :url => "/rss" }
 end
 
-Hooks::Menu.add_menu_item do
-  {:text => "RSS", :url => "/rss" }
-end
-
-Hooks::Menu.add_menu_item do
-  {:text => "ATOM", :url => "/atom" }
+Hooks::View.register_view do
+  { :name => "head", :partial => "feed_link" }
 end
