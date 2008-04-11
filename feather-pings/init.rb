c13fac480847a2153ba23f2c6f28473bc99b319d
El Draper <el@eldiablo.co.uk>
2008-04-11 21:08:49 +0100

    added a pings plugin, to allow 3rd party services to be pinged when new content is published

---
+++ feather-pings/init.rb
@@ -0,0 +1,31 @@
+require "xmlrpc/client"
+require File.join(File.join(File.dirname(__FILE__), "controllers"), "ping_logs")
+require File.join(File.join(File.dirname(__FILE__), "controllers"), "ping_services")
+require File.join(File.join(File.dirname(__FILE__), "models"), "ping_log")
+require File.join(File.join(File.dirname(__FILE__), "models"), "ping_service")
+
+Merb::Router.prepend do |r|
+  r.namespace :admin do |admin|
+    admin.resources :ping_logs
+    admin.resources :ping_services
+  end
+end
+
+Hooks::Menu.add_menu_item do
+  {:text => "Ping Services", :url => "/admin/ping_services" }
+end
+
+Hooks::Events.register_event(:after_publish_article) do |args|
+  PingService.all.each do |ping|
+    log = PingLog.new
+    log.ping_service_id = ping.id
+    begin
+      log.message = ping.execute(args.first)
+      log.successful = true
+    rescue Exception => err
+      log.message = err.message
+      log.successful = false
+    end
+    log.save
+  end
+end
