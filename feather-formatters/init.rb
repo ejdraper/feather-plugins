3e504c9832fad1a35843468324e09e66ef6d52e2
Michael Leung <blamemike@gmail.com>
2008-04-10 20:22:39 -0400

    Got rid of rubypants.

--- feather-formatters/init.rb
+++ feather-formatters/init.rb
@@ -2,8 +2,6 @@ gem "RedCloth"
 require "redcloth"
 gem "BlueCloth"
 require "bluecloth"
-gem "rubypants"
-require "rubypants"
 
 Hooks::Formatters.register_formatter("textile") do |text|
   RedCloth.new(text).to_html
@@ -11,12 +9,4 @@ end
 
 Hooks::Formatters.register_formatter("markdown") do |text|
   BlueCloth.new(text).to_html
-end
-
-Hooks::Formatters.register_formatter("textile+smartypants") do |text|
-  RedCloth.new(RubyPants.new(text).to_html).to_html
-end
-
-Hooks::Formatters.register_formatter("markdown+smartypants") do |text|
-  BlueCloth.new(RubyPants.new(text).to_html).to_html
 end
