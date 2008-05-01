8fad2b0febcdaec82b336cd8cf174685204ede47
Markus Prinz <markus.prinz@nuclearsquid.com>
2008-05-01 15:53:29 +0200

    Add Atom Feeds to head and sidebar

--- feather-feeds/helpers/global_helpers.rb
+++ feather-feeds/helpers/global_helpers.rb
@@ -1,8 +1,8 @@
 module Merb
   module GlobalHelpers
-    def feed_url
+    def feed_url(format = "rss")
       feed_settings = FeedSetting.current
-      (feed_settings.external_feed_url.nil? || feed_settings.external_feed_url == "") ? "http://#{request.env['HTTP_HOST']}/articles.rss" : feed_settings.external_feed_url
+      (feed_settings.external_feed_url.nil? || feed_settings.external_feed_url.empty?) ? "http://#{request.env['HTTP_HOST']}/articles.#{format}" : feed_settings.external_feed_url
     end
   end
 end
