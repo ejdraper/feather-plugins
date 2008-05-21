636870228914bfaad5ec6bd8e92300e6a5d1b4aa
Michael Murray <mdmurray@gmail.com>
2008-05-21 12:57:24 -0700

    Adding coderay support to feather-formatters via markdown + coderay formatter, added a stylesheet to color highlight code

--- feather-formatters/init.rb
+++ feather-formatters/init.rb
@@ -5,6 +5,13 @@ require "bluecloth"
 gem 'coderay'
 require 'coderay'
 
+def sanitize_code(text)
+  text.scan(/(<code\:([a-z].+?)>(.+?)<\/code>)/m).each do |match|
+    text.gsub!(match[0],CodeRay.scan(match[2], match[1].to_sym).div(:css => :class))
+  end
+  text
+end
+
 Hooks::Formatters.register_formatter("textile") do |text|
   RedCloth.new(text).to_html
 end
@@ -13,9 +20,8 @@ Hooks::Formatters.register_formatter("markdown") do |text|
   BlueCloth.new(text).to_html
 end
 
-Hooks::Formatters.register_formatter('coderay') do |text|
-  text.scan(/(<code\:([a-z].+?)>(.+?)<\/code>)/m).each do |match|
-    text.gsub!(match[0],CodeRay.scan(match[2], match[1].to_sym).div(:css => :class))
-  end
-  text
+Hooks::Formatters.register_formatter('markdown + coderay') do |text|
+  sanitize_code(BlueCloth.new(text)).to_html
 end
+
+Hooks::View.register_partial_view "head", 'coderay'
