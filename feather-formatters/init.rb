da174f01614e010c4b3bc439498ff2a85b4479f6
Michael Murray <mdmurray@gmail.com>
2008-05-20 21:23:12 -0700

    Adding coderay support to feather-formatters - conflict with coderay/helpers/plugin.rb? - 'Plugin is not a module'

--- feather-formatters/init.rb
+++ feather-formatters/init.rb
@@ -2,6 +2,8 @@ gem "RedCloth"
 require "redcloth"
 gem "BlueCloth"
 require "bluecloth"
+gem 'coderay'
+require 'coderay'
 
 Hooks::Formatters.register_formatter("textile") do |text|
   RedCloth.new(text).to_html
@@ -9,4 +11,11 @@ end
 
 Hooks::Formatters.register_formatter("markdown") do |text|
   BlueCloth.new(text).to_html
+end
+
+Hooks::Formatters.register_formatter('coderay') do |text|
+  text.scan(/(<code\:([a-z].+?)>(.+?)<\/code>)/m).each do |match|
+    text.gsub!(match[0],CodeRay.scan(match[2], match[1].to_sym).div(:css => :class))
+  end
+  text
 end
