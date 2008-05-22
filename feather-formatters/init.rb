60cedb60cb04aba12d080546f8b11a23720f19ba
Michael Murray <mdmurray@gmail.com>
2008-05-22 09:20:36 -0700

    Uncommenting coderay support

--- feather-formatters/init.rb
+++ feather-formatters/init.rb
@@ -4,15 +4,15 @@ gem "BlueCloth"
 require "bluecloth"
 gem "rubypants"
 require "rubypants"
-#gem 'coderay'
-#require 'coderay'
+gem 'coderay'
+require 'coderay'
 
-#def sanitize_code(text)
-#  text.scan(/(<code\:([a-z].+?)>(.+?)<\/code>)/m).each do |match|
-#    text.gsub!(match[0],CodeRay.scan(match[2], match[1].to_sym).div(:css => :class))
-#  end
-#  text
-#end
+def sanitize_code(text)
+  text.scan(/(<code\:([a-z].+?)>(.+?)<\/code>)/m).each do |match|
+    text.gsub!(match[0],CodeRay.scan(match[2], match[1].to_sym).div(:css => :class))
+  end
+  text
+end
 
 Hooks::Formatters.register_formatter("textile") do |text|
   RedCloth.new(text).to_html
@@ -22,8 +22,8 @@ Hooks::Formatters.register_formatter("markdown") do |text|
   RubyPants.new(BlueCloth.new(text).to_html).to_html
 end
 
-#Hooks::Formatters.register_formatter('markdown + coderay') do |text|
-#  sanitize_code(BlueCloth.new(text)).to_html
-#end
+Hooks::Formatters.register_formatter('markdown + coderay') do |text|
+  sanitize_code(BlueCloth.new(text)).to_html
+end
 
-#Hooks::View.register_partial_view "head", 'coderay'
+Hooks::View.register_partial_view "head", 'coderay'
