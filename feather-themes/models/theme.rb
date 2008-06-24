5bfc80e6ca7b9017dfe36fa486c22ea3b224f98b
Alexander Flatter <flatter@alexander-flatters-macbook-pro.local>
2008-06-24 12:20:18 +0200

    Assets support for feather-themes; Improved installation

--- feather-themes/models/theme.rb
+++ feather-themes/models/theme.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class Theme
   
   attr_accessor :name, :author, :version, :url, :about
@@ -19,7 +21,29 @@ class Theme
   end
   
   def destroy
+    remove_assets
     FileUtils.rm_rf(path)
+  end
+  
+  def each_asset_path(&proc)
+    [:stylesheet => "stylesheets", :image => "images", :javascript => "javascripts"].each_pair do |k, v|
+      source = File.join(path, 'assets', v)
+      target = File.join(Merb.dir_for(k), manifest['name'])
+      proc.call(source, target)
+    end
+  end
+  
+  def install_assets
+    each_asset_path do |source, target|
+      FileUtils.mkdir_p(target)
+      File.mv(Dir.glob(File.join(source, '*')), target)
+    end
+  end
+  
+  def remove_assets
+    each_asset_path do |source, target|
+      FileUtils.rm_rf(target)
+    end
   end
   
   class << self
@@ -44,20 +68,42 @@ class Theme
       end
     end
     
-    def install(url)
-      # Get manifest
-      manifest = YAML::load(Net::HTTP.get(URI.parse(url + ".yml")))
-      # Target
-      path = File.join(Merb.dir_for(:themes), manifest['name'])
-      # Clean up
-      FileUtils.rm_rf(path)
-      Dir.mkdir(path)
+    def install(manifest_url)
+
+      # Oh, that url seems to give us a manifest file
+      # Let's get it
+      manifest = YAML::load(Net::HTTP.get(URI.parse(manifest_url)))
+
+      # This is the plugin installation path
+      install_path = File.join(Merb.dir_for(:themes), manifest['name'])
+
+      # Remove older versions of the plugin
+      # FIXME: Uninstall/Upgrade
+      FileUtils.rm_rf(install_path)
+      Dir.mkdir(install_path)
+      
+      # The manifest file gives us the package location
+      # If it's not a full url, use the manifest's URI
+      package_url = begin 
+        uri = URI.parse(manifest['package'])
+        if uri.scheme.nil? and uri.host.nil?
+          File.join(File.dirname(manifest_url), manifest['package'])
+        else
+          manifest['package']  
+        end
+      end
+
       # Download the package and untgz
-      package = Net::HTTP.get(URI.parse(url + ".tgz"))
-      Archive::Tar::Minitar.unpack(Zlib::GzipReader.new(StringIO.new(package)), path)
+      package = Net::HTTP.get(package_url)
+      Archive::Tar::Minitar.unpack(Zlib::GzipReader.new(StringIO.new(package)), install_path)  
+      
       # Grab metadata from manifest
-      new(manifest['name'])
+      object = new(manifest['name'])
+      object.install_assets
+      object
+
     end
+    
   end
   
 end
