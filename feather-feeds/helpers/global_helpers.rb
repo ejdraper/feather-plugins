5062b6b73a805a5eb36f3c6da9a5b50493d5541d
AJ Christensen <aj@junglist.gen.nz>
2008-07-05 22:59:50 +1200

    Forgot one - .env['HTTP_HOST'] -> .host

--- feather-feeds/helpers/global_helpers.rb
+++ feather-feeds/helpers/global_helpers.rb
@@ -2,7 +2,7 @@ module Merb
   module GlobalHelpers
     def feed_url(format = "rss")
       feed_settings = FeedSetting.current
-      (feed_settings.external_feed_url.nil? || feed_settings.external_feed_url.empty?) ? "http://#{request.env['HTTP_HOST']}/articles.#{format}" : feed_settings.external_feed_url
+      (feed_settings.external_feed_url.nil? || feed_settings.external_feed_url.empty?) ? "http://#{request.host}/articles.#{format}" : feed_settings.external_feed_url
     end
   end
 end
