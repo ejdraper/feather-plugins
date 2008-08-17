25d2c1b51eefbe24b1bfc5b178e25509df0332a0
AJ Christensen <aj@junglist.gen.nz>
2008-08-17 18:57:37 +1200

    Added an index route to the Tags controller, /tags. Reenabled the tag cloud in the sidebar, hopefully.

--- feather-tagging/init.rb
+++ feather-tagging/init.rb
@@ -1,18 +1,15 @@
 require 'dm-tags'
 require File.join(File.join(File.dirname(__FILE__), "controllers"), "tags")
 require File.join(File.join(File.dirname(__FILE__), "models"), "article")
+require File.join(File.join(File.dirname(__FILE__), "lib"), "global_helpers")
 
 include Merb::Cache::ControllerInstanceMethods
 
 Merb::Router.prepend do |r|
-  r.match('/tags/:id').to(:controller => 'tags', :action =>'show').name(:tag)
+  r.match('/tags').to(:controller => 'tags', :action => 'index').name(:tagindex)
+  r.match('/tag/:id').to(:controller => 'tags', :action =>'show').name(:tag)
 end
 
 Hooks::View.register_partial_view "article_form_fields", "tag_field"
 Hooks::View.register_partial_view "meta_section", "tag_list"
-#Hooks::View.register_partial_view "sidebar", "tag_cloud"
-
-#Hooks::Events.register_event(:after_save_article) do |args|
-#  args.first.create_tags
-#  expire_all_pages
-#end
+Hooks::View.register_partial_view "sidebar", "tag_cloud"
---
+++ feather-tagging/lib/global_helpers.rb
@@ -0,0 +1,18 @@
+module Merb
+  module GlobalHelpers    
+    def tag_cloud(tags, classes)
+      return "" if tags.nil? || tags.empty?
+      max, min = 0, 0
+      tags.each do |t|
+        count = Article.tagged_with(t.name).count.to_i
+        max = count if count > max
+        min = count if count < min
+      end
+      divisor = ((max - min) / classes.size) + 1
+      tags.each do |t|
+        count = Article.tagged_with(t.name).count.to_i
+        yield t, classes[count / divisor]
+      end
+    end
+  end
+end
