92c3524189e9e7b1316d2864b422a584660b901d
El Draper <el@eldiablo.co.uk>
2009-01-02 02:48:25 +0000

    this changes feeds to use ERB instead of Builder for the feeds, which seems to resolve compatibility issues with Passenger/Rack; also, commenting out the head/sidebar views for the minute as they aren't working fully

--- feather-feeds/helpers/global_helpers.rb
+++
@@ -1,8 +0,0 @@
-module Merb
-  module GlobalHelpers
-    def feed_url(format)
-      feed_settings = ::Feather::FeedSetting.current
-      (feed_settings.external_feed_url.nil? || feed_settings.external_feed_url.empty?) ? url(:feeds, :action => :articles, :format => format) : feed_settings.external_feed_url
-    end
-  end
-end
--- feather-feeds/init.rb
+++ feather-feeds/init.rb
@@ -1,11 +1,6 @@
-gem "builder"
-require "builder"
 require File.join(File.dirname(__FILE__), "controllers", "feeds")
 require File.join(File.dirname(__FILE__), "controllers", "admin", "feed_settings")
-require File.join(File.dirname(__FILE__), "helpers", "global_helpers")
 require File.join(File.dirname(__FILE__), "models", "feather", "feed_setting")
-
-include Merb::GlobalHelpers
 
 Feather::Hooks::Routing.register_route do |r|
   r.match("/feeds/:action.:format").to(:controller => "feeds").name(:feeds)
@@ -14,8 +9,8 @@ Feather::Hooks::Routing.register_route do |r|
   end
 end
 
-Feather::Hooks::View.register_partial_view "head", "feed_link"
-Feather::Hooks::View.register_partial_view "sidebar", "feed_link"
+#Feather::Hooks::View.register_partial_view "head", "feed_link"
+#Feather::Hooks::View.register_partial_view "sidebar", "feed_link"
 
 Feather::Hooks::Menu.add_menu_item "Feed Settings", "/admin/feed_setting"
 
