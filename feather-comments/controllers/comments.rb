03f5dadaa1eba4294a7b1b4d268f1a277318e68e
El Draper <el@ejdraper.com>
2009-05-23 00:09:42 +0100

    fix for redirect after comment posting when running Feather as a slice

--- feather-comments/controllers/comments.rb
+++ feather-comments/controllers/comments.rb
@@ -7,7 +7,7 @@ module Feather
         @comment.save
         @article = Article[@comment.article_id]
       end
-      redirect @article.nil? ? request.env["HTTP_REFERER"] : @article.permalink
+      redirect @article.nil? ? request.env["HTTP_REFERER"] : (!::Feather.config[:path_prefix].empty? ? "/#{::Feather.config[:path_prefix]}#{@article.permalink}" : @article.permalink)
     end
   end
 end
